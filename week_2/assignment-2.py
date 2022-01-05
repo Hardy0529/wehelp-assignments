7bf53cb24459afa4e7660381cd7f15bb55343b99
李杰儒 Jeff <ijieru41@gmail.com>
2022-01-05 19:59:46 +0800

    新增測試資料

--- week_2/assignment-2.py
+++ week_2/assignment-2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python     assignment-2.py
+#!/usr/bin/python   
 # -*- coding: utf-8 -*-
 
 # 1
@@ -57,6 +57,7 @@ maxProduct([5, 20, 2, 6]) # 得到 120
 maxProduct([10, -20, 0, 3]) # 得到 30
 maxProduct([-1, 2]) # 得到 -2
 maxProduct([-1, 0, 2]) # 得到 0
+maxProduct([-1, 0, 2]) # 得到 0
 
 
 #4
@@ -66,7 +67,7 @@ def twoSum(nums, target):
         for j in range(0 ,x):
             if nums[i] + nums[j] == target:
                 return nums[i] , nums[j]
-        
+
 # your code here
 result=twoSum([2, 11, 7, 15], 9)
 print(result) # show [0, 2] because nums[0]+nums[2] is 9
